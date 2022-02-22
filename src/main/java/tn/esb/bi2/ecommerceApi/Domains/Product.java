ffef7b210e89a9a6d5a9ac527fcd8ab85b562972
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-02-22 09:39:52 +0100

    Implement product and Provider

--- src/main/java/tn/esb/bi2/ecommerceApi/Domains/Product.java
+++ src/main/java/tn/esb/bi2/ecommerceApi/Domains/Product.java
@@ -1,17 +1,44 @@
 package tn.esb.bi2.ecommerceApi.Domains;
 
 
-import javax.persistence.Lob;
-import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+@Getter //genère au moment du runtime tous les getters pour tous les attributs
+@Setter
+@NoArgsConstructor //genère un constructeur non parametré
+@AllArgsConstructor //genère un constructeur avec tous les paramètres
+@RequiredArgsConstructor //genère un constructeur avec les attributs obligatoires comme paramètres
+//@ToString //permet de retourner une chaine de caractères contenant les valeurs de tous les attributs
+//@ToString(exclude ={"image","fabricationDate"})//retourne les valeurs de toutes les colonnes sauf "image" et "fabricationDate"
+@ToString(exclude="image")
+//@EqualsAndHashCode //genère une redefinition de Equals et HashCode en se basant sur tous les attributs
+//@EqualsAndHashCode(exclude={"image","price","description"})
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)//la comparaison sera fait via uniquement les attributs annotés avec Include
+@Entity //La classe Product sera transformée par l'ORM : Object Relational Mapping en une table relationnelle
 public class Product {
+    @Id //l'attribut "id" est la clé primaire de la table
+    @GeneratedValue //l'attribut est automatiquement generé
     private Long id;
+    @EqualsAndHashCode.Include
+    @NonNull //label est obligatoire
     private String label;
+    @NonNull
+    @Column(precision = 3) //3 chiffres après virgules
     private double price;
+    @NonNull
+    @EqualsAndHashCode.Include
+    @JsonFormat(pattern ="yyyy-MM-dd")
     private LocalDate expirationDate;
+    @NonNull
+    @EqualsAndHashCode.Include
+    @JsonFormat(pattern ="yyyy-MM-dd")
     private LocalDate fabricationDate;
     private int stock;
     private String description;
     @Lob
     private byte[] image;
+
 }
--- src/main/java/tn/esb/bi2/ecommerceApi/Domains/Provider.java
+++ src/main/java/tn/esb/bi2/ecommerceApi/Domains/Provider.java
@@ -1,4 +1,31 @@
 package tn.esb.bi2.ecommerceApi.Domains;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+@Data  //genere getters, setters, RequiredArgsConstructor, ToString et equalsAndHashCode
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@Entity
+@Table(name = "provider_tab") //permet de personnaliser la table au moment de generation du code SQL par l'ORM
+//on peut preciser son nom (par defaut c'est le nom de la classe, les contraintes
 public class Provider {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //la valeur de l'id sera auto-incrimentée de 1 à chaque ajout en commençant de 1 aussi
+    private Long id;
+    @NonNull
+    @Column(name = "provider_name", unique = true, nullable = false,length = 100)
+    //personaliser la colonne en pricisant par example son nom, unique ou nom,...
+    private String name;
+    @NonNull
+    @Column(unique = true, nullable = false)
+    @EqualsAndHashCode.Include
+    private String email;
+    @NonNull
+    private String telephone;
+    private String siteUrl;
 }
