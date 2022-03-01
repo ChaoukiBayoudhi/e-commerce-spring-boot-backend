db6c8520ae4d900b81e57c6c12399f15a71f77ac
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-03-01 08:53:16 +0100

    Implement Address and Customer

--- src/main/java/tn/esb/bi2/ecommerceApi/Domains/Customer.java
+++ src/main/java/tn/esb/bi2/ecommerceApi/Domains/Customer.java
@@ -1,12 +1,22 @@
 package tn.esb.bi2.ecommerceApi.Domains;
 
-import javax.persistence.*;
+import lombok.*;
 
+import javax.persistence.*;
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString
+@NoArgsConstructor
 @Entity
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
 
     @OneToOne (mappedBy = "customer",cascade = CascadeType.ALL)
     @JoinColumn(name = "customer_address",referencedColumnName="id")
