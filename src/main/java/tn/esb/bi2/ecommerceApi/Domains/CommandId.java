de84e6fcf07165ef73cf2000405e68d317c2ddd6
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-03-01 09:45:59 +0100

    Replace @Data with generated code

---
+++ src/main/java/tn/esb/bi2/ecommerceApi/Domains/CommandId.java
@@ -0,0 +1,6 @@
+package tn.esb.bi2.ecommerceApi.Domains;
+//Cette classe sert à definir la clé primaire composite de l'entité "Command"
+public class CommandId {
+    private Long idProduct;
+    private Long idCustomer;
+}
---
+++ src/main/java/tn/esb/bi2/ecommerceApi/Enumerations/CustomerType.java
@@ -0,0 +1,7 @@
+package tn.esb.bi2.ecommerceApi.Enumerations;
+
+public enum CustomerType {
+    LOYAL,
+    VERYLOYAL,
+    NORMAL
+}
