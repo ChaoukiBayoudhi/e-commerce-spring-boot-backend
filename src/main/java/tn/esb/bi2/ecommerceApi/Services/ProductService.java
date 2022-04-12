3ab1c55dfba304df75b903906eb06f3165b6da73
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-04-12 12:07:05 +0100

    implement services and controllers

--- src/main/java/tn/esb/bi2/ecommerceApi/Services/ProductService.java
+++ src/main/java/tn/esb/bi2/ecommerceApi/Services/ProductService.java
@@ -1,4 +1,11 @@
 package tn.esb.bi2.ecommerceApi.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tn.esb.bi2.ecommerceApi.Repositories.ProductRepository;
+
+@Service
 public class ProductService {
+    @Autowired
+    private ProductRepository productRepos;
 }
--- src/main/java/tn/esb/bi2/ecommerceApi/Web/ProductController.java
+++ src/main/java/tn/esb/bi2/ecommerceApi/Web/ProductController.java
@@ -1,4 +1,11 @@
 package tn.esb.bi2.ecommerceApi.Web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import tn.esb.bi2.ecommerceApi.Services.ProductService;
+
+@RestController
 public class ProductController {
+    @Autowired
+    private ProductService productServ;
 }
