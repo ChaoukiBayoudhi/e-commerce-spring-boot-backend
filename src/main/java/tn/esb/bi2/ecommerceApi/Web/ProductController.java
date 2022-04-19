567b5d0ed1dc616c78c2fb773d7cda522bbdcd0f
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-04-19 11:43:14 +0100

    implement productService and productController

--- src/main/java/tn/esb/bi2/ecommerceApi/Web/ProductController.java
+++ src/main/java/tn/esb/bi2/ecommerceApi/Web/ProductController.java
@@ -2,10 +2,13 @@ package tn.esb.bi2.ecommerceApi.Web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tn.esb.bi2.ecommerceApi.Domains.Product;
 import tn.esb.bi2.ecommerceApi.Services.ProductService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/products")
@@ -14,7 +17,7 @@ public class ProductController {
     @Autowired
     private ProductService productServ;
     @PostMapping("/add")
-    public Product addNewProduct(Product p) throws Exception {
+    public Product addNewProduct(@Valid @RequestBody Product p) throws Exception {
         return productServ.addProduct(p);
     }
 }
