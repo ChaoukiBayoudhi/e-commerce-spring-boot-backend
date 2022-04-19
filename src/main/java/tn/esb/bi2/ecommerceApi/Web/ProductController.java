13fb62ffc60b9905ce9ce166cf8ce54ff60783d9
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-04-19 11:52:31 +0100

    implement productService and productController

--- src/main/java/tn/esb/bi2/ecommerceApi/Web/ProductController.java
+++ src/main/java/tn/esb/bi2/ecommerceApi/Web/ProductController.java
@@ -1,14 +1,12 @@
 package tn.esb.bi2.ecommerceApi.Web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tn.esb.bi2.ecommerceApi.Domains.Product;
 import tn.esb.bi2.ecommerceApi.Services.ProductService;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -17,7 +15,17 @@ public class ProductController {
     @Autowired
     private ProductService productServ;
     @PostMapping("/add")
+    //url=http://localhost:9592/products/add
     public Product addNewProduct(@Valid @RequestBody Product p) throws Exception {
         return productServ.addProduct(p);
+    }
+    @GetMapping("/all")
+    //url=http://localhost:9592/products/all
+    public List<Product> getAll()throws Exception {
+        return productServ.getAllProducts();
+    }
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id)throws Exception {
+        return productServ.getProduct(id);
     }
 }
